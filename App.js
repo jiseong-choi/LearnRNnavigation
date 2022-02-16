d34fd489476a45b2689d82b7fc06a652fbfa056e
jiseong-choi <jiseong1951@kakao.com>
2022-02-17 00:46:18 +0900

    start botton tab bar

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 
+const Tab = createBottomTabNavigator();
+
 const App = () => {
+
   return (
     <></>
   );
 };
 
-export default App;
+export default App
