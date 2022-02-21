e0efa8ec3812f6d9bb62fff32e09faad54cdd500
jiseong-choi <jiseong1951@kakao.com>
2022-02-21 21:41:34 +0900

    learning now

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 
 const Tab = createBottomTabNavigator();
 
+function HomeScreen() {
+  return (
+    <Text>Home</Text>
+  )
+}
+
+function SearchScreen() {
+  return (
+    <Text>Search</Text>
+  )
+}
+
+function NotificationScreen() {
+  return (
+    <Text>Notification</Text>
+  )
+}
+
+function MessageScreen() {
+  return (
+    <Text>Message</Text>
+  )
+}
+
 const App = () => {
 
   return (
-    <></>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName='Home' >
+        <Tab.Screen name='home' component={HomeScreen} />
+        <Tab.Screen name='Search' component={SearchScreen} />
+        <Tab.Screen name='Notification' component={NotificationScreen} />
+        <Tab.Screen name='Message' component={MessageScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
